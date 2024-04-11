9a1781ad379fcb19a94ffb5e5a7f25cd31ac0dd5
Anomalocaris <flavioborinjunior@gmail.com>
2024-04-11 18:01:29 -0300

    feat: added question props

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import Question from "./components/Question"
+import Question from "./components/Question";
 
 function App() {
   return (
     <div>
-      <Question options={[
-    "An item",
-    "A second item",
-    "A third item",
-    "A fourth item",
-    "And a fifth one",
-  ]} title={"a"}/>
+      <Question
+        options={[
+          "An item",
+          "A second item",
+          "A third item",
+          "A fourth item",
+          "And a fifth one",
+        ]}
+        title={"a"}
+      />
     </div>
   );
 }
