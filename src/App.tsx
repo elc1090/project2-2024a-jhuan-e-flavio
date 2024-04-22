253b172458990b3d084badd99f4eb9b170342676
jhuanassumpcao <jhuanassumpcao@gmail.com>
2024-04-22 14:04:29 -0300

    deplpy

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <BrowserRouter> 
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/project2-2024a-jhuan-e-flavio' element={<Home />} />
         <Route path='/question' element={<QuestionManager />} />
         <Route path='/result' element={<QuizResult />} />
         <Route path='/categories' element={<Categories />} />
